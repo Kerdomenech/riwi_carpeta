a05d94a8071852a647be62dfaef0a1852866d59e
Kerdomenech <kerlys97@gmail.com>
2026-04-22 21:39:52 -0500

    userhsem3

---
+++ userh/sem1/script.js
@@ -0,0 +1,9 @@
+
+function cambiarTexto() {
+    var parrafo = document.getElementById("texto-bienvenida");
+    parrafo.textContent = "Soy estudiante de programacion en Riwi, me gusta el diseño web y cada dia aprendo algo nuevo.";
+}
+
+function mostrarMensaje() {
+    alert("Bienvenido a mi portafolio!");
+}
